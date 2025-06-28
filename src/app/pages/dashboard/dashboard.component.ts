06167b40d3f5a8bcdd3042e927f0ff020af0e50b
Diego-Larico <zelddiryt3@gmail.com>
2025-06-28 13:20:46 -0500

    feat: show dashboard tabs based on user role

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { Usuario } from '../../models/usuario';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
   user: Usuario | null = null;
+  userRol: 'paciente' | 'medico' | 'admin' | null = null;
 
   constructor(private router: Router) {
     const userData = localStorage.getItem('usuario');
     if (userData) {
       this.user = JSON.parse(userData);
+      if (this.user && this.user.idRol === 1) this.userRol = 'admin';
+      else if (this.user && this.user.idRol === 2) this.userRol = 'medico';
+      else if (this.user && this.user.idRol === 3) this.userRol = 'paciente';
     }
   }
 
